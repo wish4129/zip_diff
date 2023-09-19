f551eb0f84284b0254b1d1297838aacf3993e61c
Kevin Mun <munkevin@gmail.com>
2023-09-19 15:52:18 +0800

    drag and drop class done

---
+++ lib/drag_drop.dart
@@ -0,0 +1,75 @@
+import 'package:cross_file/cross_file.dart';
+import 'package:desktop_drop/desktop_drop.dart';
+import 'package:flutter/material.dart';
+
+class DragNDropWidget extends StatefulWidget {
+  final String text;
+  const DragNDropWidget({super.key, required this.text});
+  @override
+  State<DragNDropWidget> createState() => _DragNDropWidgetState();
+}
+
+class _DragNDropWidgetState extends State<DragNDropWidget> {
+  final List<XFile> _list = [];
+
+  bool _dragging = false;
+
+  Offset? offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropTarget(
+      onDragDone: (detail) async {
+        setState(() {
+          _list.addAll(detail.files);
+        });
+
+        debugPrint('onDragDone:');
+        for (final file in detail.files) {
+          debugPrint('  ${file.path} ${file.name}'
+              '  ${await file.lastModified()}'
+              '  ${await file.length()}'
+              '  ${file.mimeType}');
+        }
+      },
+      onDragUpdated: (details) {
+        setState(() {
+          offset = details.localPosition;
+        });
+      },
+      onDragEntered: (detail) {
+        setState(() {
+          _dragging = true;
+          offset = detail.localPosition;
+        });
+      },
+      onDragExited: (detail) {
+        setState(() {
+          _dragging = false;
+          offset = null;
+        });
+      },
+      child: Container(
+        height: 200,
+        width: 200,
+        color: _dragging ? Colors.blue.withOpacity(0.4) : Colors.grey,
+        child: Stack(
+          children: [
+            if (_list.isEmpty)
+              Center(child: Text(widget.text))
+            else
+              Text(_list.map((e) => e.path).join("\n")),
+            if (offset != null)
+              Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  '$offset',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
